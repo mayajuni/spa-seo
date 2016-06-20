441f78185150e0d90aa6f225c8c453fdb7235e64
Kwon Dong Jun <mayajuni@compal.co.kr>
2016-06-20 10:39:28 +0900

    클래스로 변경
    port, 파일폴더, 프로토컬을 받을수 있게 변경
    
    url paramter이 있을시 따로 그 url 불러오게 변경처리

--- index.js
+++ index.js
@@ -3,47 +3,68 @@
  */
 const path = require('path');
 const http = require('http');
+const Url = require('url');
 const fs = require("fs");
 const childProcess = require('child_process');
 const phantomjs = require('phantomjs-prebuilt');
 
 const binPath = phantomjs.path;
-const port = process.env.PORT ? process.env.PORT : 9999;
-const fileDirectory = process.env.FILE_DIRECTORY ? process.env.FILE_DIRECTORY : path.join(__dirname, 'files');
-const protocol = process.env.PROTOCOL ? process.env.PROTOCOL : 'http';
-if(!process.env.FILE_DIRECTORY) {
-    if(!fs.existsSync(fileDirectory)) {
-        fs.mkdirSync(fileDirectory);
+
+class Spaseo {
+    constructor(port, fileDirectory, protocol) {
+        this.port = port | 9999;
+        this.fileDirectory = fileDirectory | path.join(__dirname, 'files');
+        this.protocol = protocol | 'http';
+        this.mkdir();
     }
-}
-http.createServer((req, res)=> {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    const htmlFile = `${encodeURIComponent(req.headers.host+req.url)}.html`;
-    const htmlFilePath = path.join(fileDirectory, htmlFile);
-    if(fs.existsSync(htmlFilePath)) {
-        console.log(0);
-        fs.readFile(htmlFilePath, (err, data) => {
-            if(err){
-                res.writeHead(404);
-                res.write("Not Found!");
+
+    mkdir() {
+        if(!fs.existsSync(this.fileDirectory)) {
+            fs.mkdirSync(this.fileDirectory);
+        }
+    }
+
+    startServer() {
+        http.createServer((req, res)=> {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            const params = Url.parse(req.url,true);
+            let url = req.headers.host+req.url;
+            if(params.query.url) {
+                url = params.query.url.replace('http://', '').replace('https://', '');
             }
-            res.write(data);
-            res.end();
-        })
-    }else{
-        const childArgs = [
-            path.join(__dirname, 'src', 'crawling.js'),
-            `${protocol}://${(req.headers.host+req.url)}`
-        ];
-        childProcess.execFile(`${binPath}`, childArgs, (err, stdout, stderr) => {
-            if(!(stdout === 'bad_network' || stdout === 'need_url')) {
-                fs.writeFileSync(htmlFilePath, stdout, 'utf8');
+            const htmlFile = `${encodeURIComponent(url)}.html`;
+            const htmlFilePath = path.join(this.fileDirectory, htmlFile);
+
+            if(fs.existsSync(htmlFilePath)) {
+                fs.readFile(htmlFilePath, (err, data) => {
+                    if(err){
+                        res.writeHead(404);
+                        res.write("Not Found!");
+                    }
+                    res.write(data);
+                    res.end();
+                })
+            }else{
+                const childArgs = [
+                    path.join(__dirname, 'src', 'crawling.js'),
+                    `${this.protocol}://${url}`
+                ];
+                childProcess.execFile(`${binPath}`, childArgs, (err, stdout) => {
+                    if(stdout){
+                        if(!(stdout === 'bad_network' || stdout === 'need_url' || stdout === 'timeout')) {
+                            fs.writeFileSync(htmlFilePath, stdout, 'utf8');
+                        } else {
+                            res.writeHead(400);
+                        }
+                        res.write(stdout);
+                        res.end();
+                    }
+                });
             }
-            res.write(stdout);
-            res.end();
+        }).listen(this.port, () => {
+            console.log(`Start server ${port}`);
         });
     }
-}).listen(port, () => {
-    console.log(`Start server ${port}`);
-});
+}
 
+exports.Spaseo = Spaseo;
